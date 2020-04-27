ccf9f7b28799a88dd47e8fd35aba818fb59e2ec1
martina-borisova <borisova.martina760@gmail.com>
2020-04-27 20:20:24 +0300

    Section 16 completed - image upload API

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -6,7 +6,9 @@ namespace Domain
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
+        public string Bio { get; set; }
         // virtual - uses lazy loading 
         public virtual ICollection<UserActivity> UserActivities { get; set; }
+        public virtual ICollection<Photo> Photos { get; set; }
     }
 }
